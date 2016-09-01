1ced33f64d8d5a3a8a4839ba70a02ac19f96fdfa
green <green@greens-Mac-mini.local>
2016-09-01 11:39:05 -0500

    Working Digital Ocean provisioning example

--- README.md
+++ README.md
@@ -7,6 +7,12 @@ Digital Ocean account created
 Digital Ocean API key obtained  
 Digital Ocean SSH public key uploaded with fingerprint obtained  
 
+# References
+### Terraform usage
+https://www.digitalocean.com/community/tutorials/how-to-use-terraform-with-digitalocean  
+https://github.com/hashicorp/terraform/tree/master/examples/digitalocean
+### Vault usage
+
 # Usage
-Copy secrets.tf to secrets.tf.example  
+Copy secret.tf.example to secret.tf  
 Update the values in all caps with your specific information  
--- main.tf
+++ main.tf
@@ -1,8 +1,26 @@
+provider "digitalocean" {
+  token = "${var.do_token}"
+}
 
+resource "digitalocean_droplet" "testserver" {
+  ssh_keys = ["${var.ssh_keys}"]
+	image = "${var.ubuntu}"
+	region = "${var.do_tor1}"
+	size = "2gb"
+	private_networking = true
+	name = "testserver"
 
-
-
-resource "digitalocean_ssh_key" "default" {
-    name = "Terraform Example"
-    public_key = "${file("/Users/terraform/.ssh/id_rsa.pub")}"
+	provisioner "remote-exec" {
+		inline = [
+		"export PATH=$PATH:/usr/bin",
+		"sudo apt-get update",
+		"sudo apt-get -y install nginx"
+		]
+		connection {
+			type = "ssh"
+      key_file = "${var.key_file}"
+			user = "root"
+			timeout = "2m"
+		}
+	}
 }
